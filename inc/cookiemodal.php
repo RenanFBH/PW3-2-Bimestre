acc169c5fa3ff0030a876f81969f57d7579535fa
Renan <Renan.f.bezerra10@gmail.com>
2025-08-31 16:24:24 -0300

    Versão 12

---
+++ inc/cookiemodal.php
@@ -0,0 +1,31 @@
+<div class="modal-dialog modal-frame modal-bottom fixed-bottom text-white-50 bg-dark msg-cookies" id="modal-cookies">
+    <div class="modal-content rounded-0">
+        <div class="modal-body py-1">
+            <div class="d-flex justify-content-center align-items-center my-3">
+                <p class="mb-0 px-2">
+                    <i class="fas fa-cookie-bite fa-2x text-white-50"></i> Nós usamos cookies para melhorar sua experiência no site.
+                </p>
+                <button type="button" class="btn btn-secondary btn-sm ms-2 active" onclick="aceitar()">Ok, Obrigado</button>
+				<button type="button" class="btn btn-secondary btn-sm ms-2" onclick="recusar()">Recusar</button>
+                <a class="btn btn-secondary btn-sm ms-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Detalhes</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+				<div class="modal-body">
+				...
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Understood</button>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ js/cookies.js
@@ -0,0 +1,10 @@
+const msgCookies = document.getElementById('modal-cookies');
+	
+function aceitar() {
+	console.log("aceito");
+}
+	
+function recusar() {
+	console.log("recuso");
+}
+
